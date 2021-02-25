e20a9d8559d02859971d0587a7c75258f30ad986
Andrew Beveridge <andrew@beveridge.uk>
2021-02-25 16:44:29 -0500

    Added serverless mysql database, connectable from EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,12 @@ terraform {
 }
 
 locals {
-  region = "eu-west-2"
+  region = "eu-west-1"
   name   = "bimtwin-snipe"
 
   db_name     = "snipe"
-  db_password = random_password.password.result
+  db_username = random_pet.random-pet.id
+  db_password = random_password.random-password.result
 
   tags = {
     Name                   = "upwork-bimtwin-terraform"
@@ -26,7 +27,12 @@ provider "aws" {
   region  = local.region
 }
 
-resource "random_password" "password" {
+resource "random_pet" "random-pet" {
+  length    = "2"
+  separator = "_"
+}
+
+resource "random_password" "random-password" {
   length  = 20
   special = false
 }
@@ -130,9 +136,7 @@ resource "aws_instance" "ubuntu-instance" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.ubuntu-subnet.id
 
-  vpc_security_group_ids = [
-    aws_security_group.ubuntu-ingress.id
-  ]
+  vpc_security_group_ids = [aws_security_group.ubuntu-ingress.id]
 
   connection {
     type        = "ssh"
@@ -141,10 +145,23 @@ resource "aws_instance" "ubuntu-instance" {
     host        = self.public_ip
   }
 
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
   ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_type = "gp2"
-    volume_size = 30
+    delete_on_termination = true
+    device_name           = "/dev/sda1"
+    encrypted             = false
+    tags                  = {}
+    volume_size           = 30
+    volume_type           = "gp2"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    tags                  = {}
   }
 
   tags = local.tags
@@ -155,4 +172,112 @@ resource "aws_eip" "ubuntu-eip" {
   instance = aws_instance.ubuntu-instance.id
 
   tags = local.tags
+}
+
+
+resource "aws_security_group" "snipe-db-ingress" {
+  name        = "snipe-db-security-group"
+  description = "Allow HTTP, HTTPS and MySQL traffic from anywhere"
+
+  vpc_id = aws_vpc.ubuntu-vpc.id
+
+  ingress {
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+resource "aws_subnet" "snipe-db-subnet-one" {
+  cidr_block        = cidrsubnet(aws_vpc.ubuntu-vpc.cidr_block, 3, 2)
+  vpc_id            = aws_vpc.ubuntu-vpc.id
+  availability_zone = "${local.region}a"
+  tags              = local.tags
+}
+
+resource "aws_subnet" "snipe-db-subnet-two" {
+  cidr_block        = cidrsubnet(aws_vpc.ubuntu-vpc.cidr_block, 3, 3)
+  vpc_id            = aws_vpc.ubuntu-vpc.id
+  availability_zone = "${local.region}b"
+  tags              = local.tags
+}
+
+resource "aws_route_table_association" "snipe-db-subnet-one-route-table-association" {
+  subnet_id      = aws_subnet.snipe-db-subnet-one.id
+  route_table_id = aws_route_table.ubuntu-route-table.id
+}
+
+resource "aws_route_table_association" "snipe-db-subnet-two-route-table-association" {
+  subnet_id      = aws_subnet.snipe-db-subnet-two.id
+  route_table_id = aws_route_table.ubuntu-route-table.id
+}
+
+resource "aws_db_subnet_group" "snipe-db-subnet-group" {
+  name       = "${local.name}-subnet-group"
+  subnet_ids = [aws_subnet.snipe-db-subnet-one.id, aws_subnet.snipe-db-subnet-two.id]
+
+  tags = local.tags
+}
+
+resource "aws_rds_cluster" "snipe-db-cluster" {
+  cluster_identifier     = "snipe-db-cluster"
+  availability_zones     = ["${local.region}a", "${local.region}b"]
+  engine                 = "aurora-mysql"
+  engine_mode            = "serverless"
+  engine_version         = "5.7.mysql_aurora.2.07.1"
+  port                   = "3306"
+  database_name          = local.db_name
+  master_username        = local.db_username
+  master_password        = local.db_password
+  enable_http_endpoint   = true
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.snipe-db-subnet-group.name
+  vpc_security_group_ids = [aws_security_group.snipe-db-ingress.id]
+}
+
+
+output "instance_dns_addr" {
+  value = aws_instance.ubuntu-instance.public_dns
+}
+output "instance_eip_dns_addr" {
+  value = aws_eip.ubuntu-eip.public_dns
+}
+output "snipe_db_endpoint" {
+  value = aws_rds_cluster.snipe-db-cluster.endpoint
+}
+output "snipe_db_username" {
+  value = aws_rds_cluster.snipe-db-cluster.master_username
+}
+output "snipe_db_password" {
+  value = aws_rds_cluster.snipe-db-cluster.master_password
+}
+output "snipe_db_port" {
+  value = aws_rds_cluster.snipe-db-cluster.port
 }
