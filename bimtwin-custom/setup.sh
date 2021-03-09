4c6923c030ccb01e12ac31a8ee6dabbd1bb7fac0
Andrew Beveridge <andrew@beveridge.uk>
2021-03-09 18:45:20 -0500

    Created setup script to wipe/migrate DB, create admin user and settings row to skip setup flow

---
+++ bimtwin-custom/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker exec -it snipe php artisan migrate:fresh
+
+docker exec -it snipe php artisan snipeit:create-admin --first_name=Andrew --last_name=Beveridge \
+  --email=admin@bimtwin.ml --username=admin --password=bimtwin123
+
+mysql -h 127.0.0.1 -u snipe -psnipemysqlpassword snipe < settings.sql
