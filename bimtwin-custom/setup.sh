b1d33230d4e82343e03c2472f20c6b225d3b8c41
Andrew Beveridge <andrew@beveridge.uk>
2021-03-09 18:57:59 -0500

    Fixed filepath in setup script, added automated migration file fixes after running artisan migrate

--- bimtwin-custom/setup.sh
+++ bimtwin-custom/setup.sh
@@ -5,4 +5,4 @@ docker exec -it snipe php artisan migrate:fresh
 docker exec -it snipe php artisan snipeit:create-admin --first_name=Andrew --last_name=Beveridge \
   --email=admin@bimtwin.ml --username=admin --password=bimtwin123
 
-mysql -h 127.0.0.1 -u snipe -psnipemysqlpassword snipe < settings.sql
+mysql -h 127.0.0.1 -u snipe -psnipemysqlpassword snipe < ./bimtwin-custom/settings.sql
